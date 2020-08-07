2bd26f465e134b52914ea7c72dc8d02cf8f81d0d
Bhanu <bpst07@gmail.com>
2020-08-07 16:27:21 +0530

    Updated GeoLocation

--- app/src/main/java/com/arrow/java/project/useronmap/MapsActivity.java
+++ app/src/main/java/com/arrow/java/project/useronmap/MapsActivity.java
@@ -8,11 +8,14 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -21,6 +24,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -82,6 +89,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(userLocation).title("CurrentLocation"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    // For Current Location
+                    List<Address> addressList = geocoder.getFromLocation(
+                            location.getLatitude(), location.getLongitude(), 1);
+                    if (addressList != null && addressList.size() > 0) {
+                        String address = "";
+                        if(addressList.get(0).getThoroughfare() != null) {
+                            address += addressList.get(0).getThoroughfare();
+                        }
+                        if(addressList.get(0).getLocality() != null) {
+                            address += addressList.get(0).getLocality();
+                        }
+                        if(addressList.get(0).getPostalCode() != null) {
+                            address += addressList.get(0).getPostalCode();
+                        }
+                        Toast.makeText(MapsActivity.this, address, Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         };
 
